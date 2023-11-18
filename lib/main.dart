cf609a00274245a30429fbdc7ca26b375c751bc9
foryoudrizzle14 <foryoudrizzle14@gmail.com>
2023-11-18 21:07:56 +0900

    background and text

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 147, 91, 243),
+                Color.fromARGB(255, 116, 44, 241),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
